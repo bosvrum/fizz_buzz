0e16ae5e3481aa1894166b3cb46a0ad01cafed4d
Catalin <catalinboruga@yahoo.com>
2014-07-29 22:30:50 +0200

    My first solution of fizz_buzz in ruby

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,4 +5,19 @@ describe "FizzBuzz" do
     expect(fizzbuzz(1)).to eq(1)
     expect(fizzbuzz(2)).to eq(2)
   end
+
+  it "replies fizz" do
+    expect(fizzbuzz(3)).to eq('Fizz')
+    expect(fizzbuzz(6)).to eq('Fizz')
+    expect(fizzbuzz(9)).to eq('Fizz')
+  end
+
+  it "replies buzz" do
+    expect(fizzbuzz(5)).to eq('Buzz')
+    expect(fizzbuzz(10)).to eq('Buzz')
+  end
+
+  it "replies fizzbuzz" do
+    expect(fizzbuzz(15)).to eq('FizzBuzz')
+  end
 end
